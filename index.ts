dd3c74ac3dab95c2e29afab30721f954050f9da3
Samuel S. Siegmeister <siegmeister.s@husky.neu.edu>
2018-03-30 12:01:47 -0400

    Added module exports

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from './src/app/components/w-mat-timepicker/w-mat-timepicker.component';
+export * from './src/app/components/w-time-dialog/w-time-dialog.component';
+export * from './src/app/components/w-clock/w-clock.component';
+export * from './src/app/components/w-time/w-time.component';
+
+export { Angular5TimePickerModule } from './src/app/components/w-mat-timepicker/w-mat-timepicker.module';
